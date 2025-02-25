3d3853aa48ef956247f68c8ef0eaae9aab6c79cb
@Veronike5 <atassieromario5@gmail.com>
2025-02-25 22:12:02 +0100

    ajour

---
+++ src/component/home.tsx
@@ -0,0 +1,16 @@
+import Home from'./home.css';
+ function home() {
+    return (
+         <div>
+             <header>
+                  <ul className='ele'>
+                     <li><a href="">Découvrir Populaires</a></li>
+                     <li><a href="">Gérald Darmanin</a></li>
+                     <li><a href="">Actualités</a></li>
+                  </ul>
+
+             </header>
+        </div>
+    );
+  }
+  export default home
